ed7c291325c70fa2f48fef81d51a13d3cf5c65b5
Martin Honnen <martin.honnen@gmx.de>
2022-05-01 13:53:57 +0200

    add ViewModel with property and command

---
+++ Roster.Client/HomeViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Roster.Client.ViewModels
+{
+    public class HomeViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Title
+        {
+            get;
+            set;
+        } = "Roster App";
+
+        public Command UpdateApplicationCommand
+        {
+            get;
+        }
+    }
+}
