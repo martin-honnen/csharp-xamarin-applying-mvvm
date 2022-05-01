d203bce1738b61a26430c4a08487cb1ffc37f784
Martin Honnen <martin.honnen@gmx.de>
2022-05-01 14:18:30 +0200

    Implement command and PropertyChanged handler

--- Roster.Client/HomeViewModel.cs
+++ Roster.Client/HomeViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 using Xamarin.Forms;
 
@@ -8,13 +9,34 @@ namespace Roster.Client.ViewModels
 {
     public class HomeViewModel : INotifyPropertyChanged
     {
+        public HomeViewModel()
+        {
+            UpdateApplicationCommand = new Command(() => Title = "Roster App (v2.0)");
+        }
         public event PropertyChangedEventHandler PropertyChanged;
 
+        string title = "Roster App";
         public string Title
         {
-            get;
-            set;
-        } = "Roster App";
+            get
+            {
+                return title;
+            }
+            set
+            {
+                if (value != title)
+                {
+                    title = value;
+                    NotifyPropertyChanged("Title");
+
+                }
+            }
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
 
         public Command UpdateApplicationCommand
         {
